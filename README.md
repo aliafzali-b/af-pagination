ce36d054b239bdc3dd8fd2f7274941bd43fafc01
ali afzali <aliafzali.b@gmail.com>
2023-04-04 16:29:50 +0330

    update

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# Run the following commands
+# a simple pagination
 
-1. npm run build
-2. npm pack
-
-3. npm uninstall af-pagination --legacy-peer-deps
-4. npm install ./packages/af-pagination-x.x.x.tgz --legacy-peer-deps
+npm install af-pagination
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,91 @@
 import React, { FC } from "react";
-type props = {};
-const index: FC<props> = (props) => {
-  return <>initial commit</>;
+import styles from "./styles/styles.module.scss";
+type props = {
+  total_count?: number;
+  per_page?: number;
+  total_pages?: number;
+  current_page: any;
+  set_page: any;
 };
-export default index;
+const AF_Pagination: FC<props> = ({
+  per_page,
+  current_page, //1,2,3,...
+  set_page,
+  total_count,
+  total_pages,
+}) => {
+  let totalPage = 1;
+  if (total_pages) totalPage = Math.ceil(total_pages);
+  if (total_count && per_page) totalPage = Math.ceil(total_count / per_page);
+  if (totalPage <= 1) return null;
+  let valid_pages = createArray(totalPage);
+  let allowedPages = [
+    1,
+    2,
+    current_page - 1,
+    current_page,
+    current_page + 1,
+    totalPage - 1,
+    totalPage,
+  ];
+  let finalPages = valid_pages.filter((page) => allowedPages.includes(page));
+  return (
+    <div className={`${styles["pagination-container"]}`}>
+      {current_page > 1 && (
+        <button
+          className={`${styles["single-pagination-nav-button"]} ${styles["prev-page-button"]}`}
+          onClick={() => current_page > 1 && set_page(current_page - 1)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+            />
+          </svg>
+        </button>
+      )}
+      {finalPages.map((page) => (
+        <button
+          key={`af-pagination-page-${page}`}
+          className={`${styles["single-pagination-button"]} ${
+            styles["prev-page-button"]
+          } ${current_page === page ? styles["active"] : ""}`}
+          onClick={() => current_page !== page && set_page(page)}
+        >
+          {page}
+        </button>
+      ))}
+      {current_page !== totalPage && (
+        <button
+          className={`${styles["single-pagination-nav-button"]} ${styles["next-page-button"]}`}
+          onClick={() => current_page > 1 && set_page(current_page - 1)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+            />
+          </svg>
+        </button>
+      )}
+    </div>
+  );
+};
+export default AF_Pagination;
+function createArray(length: number): number[] {
+  return Array.from({ length }, (_v, i) => i + 1);
+  //const myArray = createArray(6)
+  // Output: [1, 2, 3, 4, 5, 6]
+}
